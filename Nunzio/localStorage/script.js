2042a52ac515bc0bd158105fc1b08811536d0345
giaba90 <--global>
2020-06-19 14:49:04 +0200

    consegna progetto

--- Nunzio/localStorage/script.js
+++ Nunzio/localStorage/script.js
@@ -3,16 +3,14 @@ let anagrafia = [];
 let nome;
 let cognome;
 let codiceFiscale;
-let cf_exist = false;
 let countUser = 0;
-
+let table = document.getElementById('table');
 //funzione prototipo
 function Persona(n, c, cf) {
     this.nome = n;
     this.cognome = c;
     this.codiceFiscale = cf;
 }
-
 //init dell'applicazione
 window.onload = () => {
     if (window.localStorage.getItem('utenti') != null) { //verifico che ci siano persone nello storage
@@ -24,25 +22,61 @@ window.onload = () => {
 
 }
 
-function displayTable(persona) {
-    const markup = ` <table class="table">
-                    <thead class="thead-dark">
-                        <tr>
-                            <th scope="col">Nome</th>
-                            <th scope="col">Cognome</th>
-                            <th scope="col">Codice fiscale</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td id="name-table">${persona.nome}</td>
-                            <td id="surname-table">${persona.cognome}</td>
-                            <td id="cf-table">${persona.codiceFiscale}</td>
-                        </tr>
-                       
-                    </tbody>
-                </table> `;
-    return markup;
+//input array di persone
+//output un markup html
+function displayTable(persone) {
+    table.innerHTML = '';
+    for (i = 0; i < persone.length; i++){
+        let node = document.createElement('div');
+        node.classList.add('list-group-item');
+        node.id = persone[i].codiceFiscale;
+        node.innerHTML = persone[i].nome + ' '+ persone[i].cognome +' '+ persone[i].codiceFiscale + '<i class="fa fa-trash" aria-hidden="true" onclick=elimina("'+persone[i].codiceFiscale+'")></i>';
+        table.appendChild(node);    
+    }
+
+}
+
+//input id della row
+function displayRow(id) {
+   
+    table.innerHTML= '';
+     let node = document.createElement('div');
+    node.classList.add('list-group-item');
+    node.id = anagrafia[id].codiceFiscale;
+    node.innerHTML = anagrafia[id].nome + ' ' + anagrafia[id].cognome + ' ' + anagrafia[id].codiceFiscale + '<i class="fa fa-trash" aria-hidden="true" onclick=elimina("' + anagrafia[id].codiceFiscale + '")></i>';
+    table.appendChild(node);
+    
+ //   return makeRow(anagrafia[id]);
+}
+
+//elimina elemento dalla anagrafia
+//input codice fiscale
+//testo dell'esito della procedura
+function elimina(cf) {
+    let confirmation = window.confirm('Sei sicuro di voler eliminare l\'utente con CF: '+ cf);
+    if (confirmation == true) {
+        let esito = false;
+        let sliced = [];
+        for (i = 0; i < anagrafia.length; i++) {
+            if (anagrafia[i].codiceFiscale == cf) {
+                anagrafia.splice(i, 1);
+                esito = true;
+            }
+        }
+        if (esito) {
+            //scrittura nello storage
+            window.localStorage.setItem('utenti', JSON.stringify(anagrafia));
+            //rimuovo l'elemento grafico dalla tabella
+            document.getElementById(cf).remove();
+            countUser -= 1;
+            document.getElementById('count').innerHTML = countUser.toString();
+            alert('Elemento cancellato')
+        }
+        else {
+            alert('Errore: non sono riuscito ad eliminare l\'elemento!');
+        }
+    
+    }
 }
 
 //salva
@@ -53,14 +87,10 @@ function salvaDB(persona) {
     anagrafia.push(persona);
     //scrittura nello storage
     window.localStorage.setItem('utenti', JSON.stringify(anagrafia));
-/*
-    document.getElementById('name-table').innerText = persona.nome;
-    document.getElementById('surname-table').innerText = persona.cognome;
-    document.getElementById('cf-table').innerText = persona.codiceFiscale;
-*/
-    console.log('caricamento utente riuscito!')
+    alert('caricamento utente riuscito!')
     countUser += 1;
     document.getElementById('count').innerHTML = countUser.toString();
+    displayTable(anagrafia);
 };
 
 //cerca
@@ -70,35 +100,41 @@ function searchDB(cf) {
     let datoLetto = JSON.parse(window.localStorage.getItem('utenti'));
     let trovato = false;
     let p;
-    //check cf only if anagrafia have an element
-    datoLetto.forEach((el) => {
-        if (el.codiceFiscale == cf) {
+
+    for (i = 0; i < datoLetto.length; i++){
+        if (datoLetto[i].codiceFiscale == cf) {
+            p = i;
             trovato = true;
-            p = el;
-        }
-    });
-   return (trovato) ? displayTable(p) : '<div class="p-3 mb-2 bg-danger text-white"><p>Utente non trovato !</p></div>';
+        }  
+    }
+
+    if (trovato) {
+        displayRow(p);
+    } else {
+        table.innerHTML = '<div class="p-3 mb-2 bg-danger text-white"><p>Utente non trovato !</p></div>';
+
+    }
 }
 
-//controlla cf
-//return false if cf exits
+//check cf - return false if cf exits
 //input codice fiscale
 //output bool value
 function check(cf) {
+    let trovato = false;
     //check cf only if anagrafia have an element
     if (anagrafia.length > 0) {
         anagrafia.forEach(function (el) {
-            if (el.codiceFiscale == cf) {
-                return cf_exist = true; //return false if cf exits
+            if (el.codiceFiscale == cf) {    
+                trovato = true; //return false if cf exits
             }
-
         });
     }
+   return (trovato) ? trovato : false;
 };
 
 
 
-document.getElementById('salva').addEventListener('click',
+document.getElementById('salvaContatto').addEventListener('submit',
     () => {
         //get value
         nome = document.getElementById('nome').value;
@@ -106,11 +142,9 @@ document.getElementById('salva').addEventListener('click',
         codiceFiscale = document.getElementById('cf').value;
         //create a object
         let p = new Persona(nome, cognome, codiceFiscale);
-
-        (!check(p.codiceFiscale)) ? salvaDB(p): console.log('ERRORE: caricamento non riuscito!');
-        //stampo
-       document.getElementById('table').innerHTML = displayTable(p);
-
+        //salvo i valori
+        (check(p.codiceFiscale)) ? alert('Attenzione: codice fiscale esistente !') : salvaDB(p) ;
+ 
         //pulisco i campi
         document.getElementById('nome').value = '';
         document.getElementById('cognome').value = '';
@@ -118,12 +152,20 @@ document.getElementById('salva').addEventListener('click',
 
     }
 );
-
+/* 
 document.getElementById('button-addon2').addEventListener('click',
     () => {
-    
-        let res = searchDB(document.getElementById('search-cf').value);
-        document.getElementById('message').innerHTML = res;
-    
+        //prendo il valore
+        let cf = document.getElementById('search-cf').value;
+        
+        searchDB(cf);
     }
-)
+)*/
+
+//update progetto secondo direttiva di Nunzio
+function searchPeople() {
+     //prendo il valore
+     let cf = document.getElementById('search-cf').value;
+  
+     searchDB(cf);
+}
