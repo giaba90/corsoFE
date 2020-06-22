0d60e7a5731f4874a7113d1fb95acde89fa56ef0
giaba90 <--global>
2020-06-22 15:21:42 +0200

    add comments

--- Nunzio/login/app.js
+++ Nunzio/login/app.js
@@ -2,10 +2,12 @@
 
 let utenti = [];
 
-// fuction //
+// main fuction //
 
-//input email, password
-//output bool
+/*
+input => email, password
+output => bool
+*/
 function matchPass(email, pwd) {
     let match = false;
     let datiStorage = JSON.parse(window.localStorage.getItem('utenti'));
@@ -17,8 +19,13 @@ function matchPass(email, pwd) {
     return match;
 }
 
+/*
+input => nome, cognome , email , password hashed
+output => bool and message
+*/
 function saveDB(n, c, email, pwd) {
-    //controlla se esiste l'email
+   //check if database is empty
+    //if is true , save immediately
     if (isEmptyDB()) {
         data = {
             nome: n,
@@ -34,7 +41,7 @@ function saveDB(n, c, email, pwd) {
             message: 'Utente registrato'
         };
     }
-    else {
+    else {//check if email exist
         if (existsEmail(email)) {
             return {
                 val: false,
@@ -61,22 +68,11 @@ function saveDB(n, c, email, pwd) {
 }
 
 
-function existsEmail(email) {
-    let trovato = false;
-    let datiStorage = JSON.parse(window.localStorage.getItem('utenti'));
-    for (i = 0; i < datiStorage.length; i++) {
-        if (datiStorage[i].email == email) {
-            trovato = true;
-        }
-    }
-    return trovato;   
-}
 
-function isEmptyDB() {
-    return ( (window.localStorage.getItem('utenti') != null) ) ? false : true;
-}
-
-//i use this function for register an user into local storage
+/*
+i use this function for register an user into local storage
+output => string message
+*/
 function regUser() {
     let nome = document.getElementById('nome').value;
     let cognome = document.getElementById('cognome').value;
@@ -96,12 +92,15 @@ function regUser() {
 
 }
 
-//i use this function for get user data from local storage and login user into my app
+/*
+i use this function for get user data from local storage and login user into my app
+output => set a cookie and redirect user
+*/
 function loginUser() {
     let email = document.getElementById('exampleInputEmail1').value;
     let password = document.getElementById('exampleInputPassword1').value;
     let passHashed = sha256(password);
-    
+
     if (!isEmptyDB()) {
         if (existsEmail(email)) {
             let res = matchPass(email, passHashed);
@@ -109,29 +108,77 @@ function loginUser() {
             if (res) {
                 //fai qualcosa
                 console.log('Login ok');
+                docCookies.setItem("app_isLogged", "true");
+                docCookies.setItem("app_email", email);
+                window.location.replace("http://localhost/corso/Nunzio/login/benvenuto.html");
             }
             else {
                 document.getElementById('errore-login').innerText = 'Errore login: password sbagliata';
 
                 setTimeout(function () {
                     document.getElementById('errore-login').innerText = '';
-                    }, 2000);     
+                }, 2000);
             }
         }
         else {
             document.getElementById('errore-login').innerText = 'Errore: email non presente nel database';
             setTimeout(function () {
                 document.getElementById('errore-login').innerText = '';
-                }, 2000);       
+            }, 2000);
         }
     }
     else {
-        document.getElementById('errore-login').innerText = 'Errore: il database è vuoto';  
+        document.getElementById('errore-login').innerText = 'Errore: il database è vuoto';
         setTimeout(function () {
-            document.getElementById('errore-login').innerText = '';  
-        }, 2000); 
+            document.getElementById('errore-login').innerText = '';
+        }, 2000);
     }
 
+}
+
+// utility function //
+
+/*
+check if an email exist
+input => email
+output => boot
+*/
+function existsEmail(email) {
+    let trovato = false;
+    let datiStorage = JSON.parse(window.localStorage.getItem('utenti'));
+    for (i = 0; i < datiStorage.length; i++) {
+        if (datiStorage[i].email == email) {
+            trovato = true;
+        }
+    }
+    return trovato;   
+}
+
+/*
+check if database is empty
+output => bool
+*/
+function isEmptyDB() {
+    return ( (window.localStorage.getItem('utenti') != null) ) ? false : true;
+}
+
+/*
+get user name and surname
+input => email
+output => obj
+*/
+function getUserInfo(email) {
+    let user = undefined;
+    let datiStorage = JSON.parse(window.localStorage.getItem('utenti'));
+    for (i = 0; i < datiStorage.length; i++) {
+        if (datiStorage[i].email == email) {
+            user = {
+                nome: datiStorage[i].nome,
+                cognome: datiStorage[i].cognome
+            }
+        }
+    }
+    return user;   
 }
 
 
@@ -141,5 +188,8 @@ window.onload = function () {
         let datiStorage = JSON.parse(window.localStorage.getItem('utenti'));
         utenti = datiStorage; //ho inizializzato l'anagrafica con i dati presenti nello storage
     }
-
+   let isLogged =  docCookies.getItem('app_isLogged');
+    if (isLogged == 'true') {
+        window.location.replace("http://localhost/corso/Nunzio/login/benvenuto.html");
+    }
 }
